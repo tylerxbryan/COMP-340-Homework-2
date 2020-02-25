264928226d88be27fddc9a9cc29a0d7d02942207
Tyler <tbryan@student.bridgew.edu>
2020-02-25 17:55:35 -0500

    Dealer class complete
    dealerTurn body filled
    InitialTurn body filled
    constructors added

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,5 +1,35 @@
 #include "Dealer.h"
 
+Dealer::Dealer()
+{
+	this->score = 0;
+	this->deck = nullptr;
+}
+
+Dealer::Dealer(Deck* deck)
+{
+	this->score = 0;
+	this->deck = deck;
+}
+
 void Dealer::dealerTurn()
 {
+	std::cout << "Dealer's face down card is " << this->hand[1]->toString() << "\n";
+	std::cout << "Dealer's score is: " << this->score << "\n";
+	while (this->score < 17) {
+		this->drawCard(deck->draw());
+		std::cout << "Dealer drew " << this->hand[hand.size() - 1]->toString() << "\n";
+		std::cout << "Dealer's score is: " << this->score << "\n";
+		if (this->score > 21) {
+			std::cout << "Dealer busts! Player wins\n";
+			return;
+		}
+	}
+}
+
+void Dealer::InitialDraw()
+{
+	this->drawCard(deck->draw());
+	this->drawCard(deck->draw());
+	std::cout << "Dealer drew " << this->hand[0]->toString() << " and face down card\n";
 }
--- Dealer.h
+++ Dealer.h
@@ -3,7 +3,13 @@
 class Dealer :
 	public Player
 {
+private:
+	Deck* deck;
 public:
+	Dealer();
+	Dealer(Deck* deck);
+	//handles logic for dealer's turn
 	void dealerTurn();
+	void InitialDraw();
 };
 
